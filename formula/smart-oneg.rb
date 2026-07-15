2d5452ccd35676b4fc8ac24eacabfe696b59ad52
Moshe <moshechaikin@users.noreply.github.com>
2026-07-15 11:59:08 -0400

    Update description for SmartOneg formula

--- formula/smart-oneg.rb
+++ formula/smart-oneg.rb
@@ -1,5 +1,5 @@
 class SmartOneg < Formula
-  desc "Shabbos & Yom Tov smart-home automation controller"
+  desc "The Ultimate Shabbos & Yom Tov Smart Home Automation App"
   homepage "https://smartoneg.com"
   url "https://github.com/moshechaikin/smart-oneg/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
